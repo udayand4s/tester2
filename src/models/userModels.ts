8eceef7e988245983d5ae16c3e8ee441a811d2f5
Coochiee <sugarytchannel@gmail.com>
2024-12-05 22:39:51 +0530

    usermodels

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -5,7 +5,29 @@ const userSchema= new mongoose.Schema({
         type: String,
         required: [true, "Please provide a username"],
         unique: true,
-    }
+    },
+    email:{
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: [true, "Please provide a password"],
+    
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgetPasswordToken: String,
+    forgetPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 })
 
 const User= mongoose.models.user || mongoose.model
